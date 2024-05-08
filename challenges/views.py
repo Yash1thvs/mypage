522bef19694205dfc6df86fcf19332d7044de4a6
Yash1thvs <yashvanthvs.learning@gmail.com>
2024-05-08 22:32:40 +0530

    - Learning DTL-Django Template Language

--- challenges/views.py
+++ challenges/views.py
@@ -21,16 +21,9 @@ monthly_challenges = {
 def index(request):
     list_items = ""
     months = list(monthly_challenges.keys())
-
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse("month-challenge", args=[month])
-        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
-
-    # "<li><a href="...">January</a></li><li><a href="...">February</a></li>..."
-
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+    return render(request, 'challenges/index.html', {
+        "months": months
+    })
 
 
 def monthly_challenge_number(request, month):
@@ -47,6 +40,8 @@ def monthly_challenge_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenges = monthly_challenges[month]
-        return HttpResponse(challenges)
+        return render(request, 'challenges/challenge.html', {
+            "text": challenges
+        })
     except:
         return HttpResponseNotFound("not supported")
